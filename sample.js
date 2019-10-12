6053b17ba337ab0da745cf1eed726d24c1d0216b
Elias Roussos <eliasisaiah@outlook.com>
2019-10-12 12:05:47 -0400

    -added an addPhoto thing

--- sample.js
+++ sample.js
@@ -25,3 +25,37 @@ bucketPromise.then(
   function(err) {
     console.error(err, err.stack);
 });
+
+function addPhoto(albumName) {
+  var files = document.getElementById("photoupload").files;
+  if (!files.length) {
+      return alert("Please choose a file to upload first.");
+  }
+  var file = files[0];
+  var fileName = file.name;
+  var albumPhotosKey = encodeURIComponent(albumName) + "//";
+
+  var photoKey = albumPhotosKey + fileName;
+
+  // Use S3 ManagedUpload class as it supports multipart uploads
+  var upload = new AWS.S3.ManagedUpload({
+      params: {
+          Bucket: albumBucketName,
+          Key: photoKey,
+          Body: file,
+          ACL: "public-read"
+      }
+  });
+
+  var promise = upload.promise();
+
+  promise.then(
+      function (data) {
+          alert("Successfully uploaded photo.");
+          viewAlbum(albumName);
+      },
+      function (err) {
+          return alert("There was an error uploading your photo: ", err.message);
+      }
+  );
+}
